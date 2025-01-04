12e6917f1b4a26d760ae3a0b13eb3bb236ba5c21
farihafari <fareehajabeen62@gmail.com>
2025-01-03 22:01:25 -0800

    cat view done

--- dashmin/php/query.php
+++ dashmin/php/query.php
@@ -1,6 +1,6 @@
 <?php
 include('connection.php');
-
+$catImageAdd = "img/categories/";
 // addcategory
 if(isset($_POST['addCategory'])){
 $catName = $_POST['catName'];
@@ -8,6 +8,26 @@ $catImageName = $_FILES['catImage']['name'];
 $fileObject = $_FILES['catImage']['tmp_name'];
 $directory = 'img/categories/'.$catImageName;
 $extension = pathinfo($catImageName, PATHINFO_EXTENSION);
+if($extension == "jpg" || $extension == "jpeg" || $extension == "png" || $extension == "webp"){
+if(move_uploaded_file($fileObject,$directory)){
+    $query = $pdo ->prepare("insert into categories(name,image)values(:pn,:pi)");
+        $query->bindParam("pn",$catName);
+        $query->bindParam("pi",$catImageName);
+        $query->execute();
+        echo "<script>
+    alert('inserted');
+    </script>";
+
+}else{
+    echo "<script>
+    alert('invaild file address');
+    </script>";
+}
+}else{
+    echo "<script>
+    alert('invaild file extension');
+    </script>";
+}
 }
 ?>
 
